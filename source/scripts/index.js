998c62eec633f9cf98f26b2f89ae10f44c6b66e5
katyakolataeva@icloud.com <katyakolataeva@icloud.com>
2025-02-14 21:23:14 +0300

    fix-day

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,22 @@
 /* в этот файл добавляет скрипты*/
+// const closeButton = document.querySelector('.header__button--clouse');
+const openButton = document.querySelector('.header__button');
+const nav = document.querySelector('nav');
+
+// Функция для удаления класса nav--cloused
+openButton.addEventListener('click', (evt) => {
+    if(nav.classList.contains('nav--cloused')) {
+        nav.classList.remove('nav--cloused');       
+        openButton.classList.add('header__button--clouse');
+
+    }else {
+     nav.classList.add('nav--cloused');
+     openButton.classList.remove('header__button--clouse');
+    }
+});
+
+// closeButton.addEventListener('click', (evt) => {
+//     closeButton.classList.remove('header__button--clouse');
+//     nav.classList.add('nav--cloused');
+// });
+// header__button--clouse добаыит когда открыто меню
